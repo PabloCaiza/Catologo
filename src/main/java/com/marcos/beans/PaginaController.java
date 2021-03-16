cc31d5847e2f9672f69d8f0c2f773467bd63c5b5
oterVs <vicentesaavedra45@gmail.com>
2021-03-15 20:20:04 -0500

    growl

--- src/main/java/com/marcos/beans/PaginaController.java
+++ src/main/java/com/marcos/beans/PaginaController.java
@@ -12,7 +12,7 @@ import javax.inject.Named;
 import com.marcos.utils.CommonUtils;
 
 @Named("paginaController")
-@ViewScoped
+@RequestScoped
 public class PaginaController  implements Serializable {
 	
 	
--- src/main/java/com/marcos/beans/RegistroProductoController.java
+++ src/main/java/com/marcos/beans/RegistroProductoController.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -110,7 +111,8 @@ public class RegistroProductoController implements Serializable {
 		identificaCategoria();
 		try {
 			servicioProducto.crear(producto);
-
+			FacesContext.getCurrentInstance().addMessage("registryForm",
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Se guardo con exito", ""));
 		} catch (Exception e) {
 
 		} finally {
